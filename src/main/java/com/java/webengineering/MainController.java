5c8295a5269ea93a690022f20a0cd1c4d3e40595
Mizantrop <sds1973@mail.ru>
2023-08-22 21:51:41 +0300

    The first controllers. dispatcher, queries

---
+++ src/main/java/com/java/webengineering/MainController.java
@@ -0,0 +1,31 @@
+package com.java.webengineering;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+   /* @GetMapping("/")
+    public String view(@RequestParam(value = "name",
+            required = false, defaultValue = "stranger") String name, Model model){
+        model.addAttribute("msg", "Hello, " + name);*/
+
+     //OR   (name is necessary)
+    @GetMapping("/{name}")
+    public String view(@PathVariable("name") String name, Model model){
+        model.addAttribute("msg", "Hello, " + name);
+    return "index";
+    }
+
+    @GetMapping("/raw")
+    @ResponseBody
+    public String raw() {
+        return "Raw data";
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Дмитрий
+  Date: 22.08.2023
+  Time: 20:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h2>${msg}</h2>
+
+</body>
+</html>
