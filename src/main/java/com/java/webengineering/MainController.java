67461b13308ce55b1fea3b45cd436bc2a13b2676
Mizantrop <sds1973@mail.ru>
2023-08-23 22:36:11 +0300

    Freemarker. List of users

--- src/main/java/com/java/webengineering/MainController.java
+++ src/main/java/com/java/webengineering/MainController.java
@@ -1,11 +1,15 @@
 package com.java.webengineering;
 
+import com.java.webengineering.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -26,6 +30,18 @@ public class MainController {
     @ResponseBody
     public String raw() {
         return "Raw data";
+    }
+
+    @GetMapping("/users")
+    public String getUsers (Model model) {
+        Collection<User> users = List.of(
+                new User("John", "Smith", "www@qqq.com"),
+                new User("Mike", "Johnson", "qqq@qqq.com")
+        );
+
+        model.addAttribute("users", users);
+        return "users";
 
     }
+
 }
---
+++ src/main/java/com/java/webengineering/model/User.java
@@ -0,0 +1,18 @@
+package com.java.webengineering.model;
+
+public class User {
+
+    private final String name;
+    private final String surname;
+    private final String email;
+
+    public User(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public String getName() {return name;}
+    public String getSurname() {return surname;}
+    public String getEmail() {return email;}
+}
